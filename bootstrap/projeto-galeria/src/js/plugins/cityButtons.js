6751e22855b619fb9b1ec1a4383cf72e76f99d13
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-11-05 13:22:36 -0300

    bug para executar os plugins

--- bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
+++ bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -1,5 +1,7 @@
 import $ from 'jquery'
 
+import { onloadHtmlSuccess } from '../core/includes'
+
 const duration = 300
 
 function filterByCity(city) {
@@ -42,4 +44,6 @@ $.fn.cityButtons = function () {
     return this
 }
 
-$('[wm-city-buttons]').cityButtons()
+onloadHtmlSuccess(function () {
+    $('[wm-city-buttons]').cityButtons()
+})
